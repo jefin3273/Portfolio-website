281179dfed17737202b203c9644ce013d6415c3b
jefin3273 <jefinjohn3273@gmail.com>
2024-12-11 17:01:56 +0530

    fix for website not being hosted

--- app/page.js
+++ app/page.js
@@ -1,13 +1,32 @@
+import dynamic from "next/dynamic";
 import { personalData } from "@/utils/data/personal-data";
-import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
 
+// Dynamically import all components with SSR: false to avoid rendering them during SSR
+const HeroSection = dynamic(
+  () => import("./components/homepage/hero-section"),
+  { ssr: false }
+);
+const AboutSection = dynamic(() => import("./components/homepage/about"), {
+  ssr: false,
+});
+const Experience = dynamic(() => import("./components/homepage/experience"), {
+  ssr: false,
+});
+const Skills = dynamic(() => import("./components/homepage/skills"), {
+  ssr: false,
+});
+const Projects = dynamic(() => import("./components/homepage/projects"), {
+  ssr: false,
+});
+const Education = dynamic(() => import("./components/homepage/education"), {
+  ssr: false,
+});
+const ContactSection = dynamic(() => import("./components/homepage/contact"), {
+  ssr: false,
+});
+const Blog = dynamic(() => import("./components/homepage/blog"), {
+  ssr: false,
+});
 async function getData() {
   try {
     const username = personalData.devUsername || "defaultUsername";
